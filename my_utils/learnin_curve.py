ad6a96ce54094ff80e676cf0366488806774ec9c
Mustafa <aweel017@gmail.com>
2025-08-14 22:10:01 +0400

    ....

--- my_utils/learnin_curve.py
+++ my_utils/learnin_curve.py
@@ -2,7 +2,11 @@ from sklearn.model_selection import learning_curve
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_learning_curve(model, X, y, train_sizes=np.linspace(0.1, 1.0, 10), cv=3, scoring='neg_mean_squared_error', ax=None):
+def plot_learning_curve(model, X, y, train_sizes=np.linspace(0.1, 1.0, 10), cv=3,
+                        scoring='neg_mean_squared_error', ax=None, show_error_bands=False,
+                         label='', model_name=''
+                        ):
+
     if ax is None:
         ax = plt.gca()
 
@@ -14,17 +18,33 @@ def plot_learning_curve(model, X, y, train_sizes=np.linspace(0.1, 1.0, 10), cv=3
         cv=cv,
         scoring=scoring,
         shuffle=True,
-        random_state=42
+        random_state=42,
+        n_jobs=-1,
     )
+    
+    training_scores_mean = np.mean(train_scores, axis=1)
+    validation_scores_mean = np.mean(val_scores, axis=1)
 
-    # Convert negative MSE to positive
-    train_errors = -np.mean(train_scores, axis=1)
-    val_errors = -np.mean(val_scores, axis=1)
+    if 'neg' in scoring:
+        training_scores_mean = -training_scores_mean
+        validation_scores_mean = -validation_scores_mean
+    
+    ax.plot(train_sizes, training_scores_mean, label=f'Training {label}', marker='o')
+    ax.plot(train_sizes, validation_scores_mean, label=f'Validation {label}', marker='s')
 
-    ax.plot(train_sizes, train_errors, label='Training MSE', marker='o')
-    ax.plot(train_sizes, val_errors, label='Validation MSE', marker='s')
-    ax.set_title(f'Learning Curve\n{str(model)}')
+    if show_error_bands:
+        trainining_scores_std = np.std(train_scores, axis=1)
+        validation_scores_std = np.std(val_scores, axis=1)
+
+        ax.fill_between(train_sizes, (training_scores_mean - trainining_scores_std), (training_scores_mean + trainining_scores_std), alpha=0.2)
+        ax.fill_between(train_sizes, (validation_scores_mean - validation_scores_std), (validation_scores_mean + validation_scores_std), alpha=0.2)
+    
+    # if model_name is None:
+    #     ax.set_title(f'Learning Curve\n{str(model)}')
+    # else:
+    #     ax.set_title(f'Learning Curve\n{model_name}')
+    ax.set_title(f'Learning Curve\n{model_name}')
     ax.set_xlabel('Training Set Size')
-    ax.set_ylabel('Mean Squared Error')
+    ax.set_ylabel(f'{label}')
     ax.legend()
     ax.grid()
